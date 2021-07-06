59ab60b3d3238a7ff91a6e1db880fa478d0be241
nagcse42 <nagcse42@gmail.com>
2021-07-06 21:33:46 +0400

    Initial setup

---
+++ src/app/analyse-data/analyse-data.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AnalyseDataComponent } from './analyse-data.component';
+
+describe('AnalyseDataComponent', () => {
+  let component: AnalyseDataComponent;
+  let fixture: ComponentFixture<AnalyseDataComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AnalyseDataComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AnalyseDataComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/analyse-data/analyse-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-analyse-data',
+  templateUrl: './analyse-data.component.html',
+  styleUrls: ['./analyse-data.component.css']
+})
+export class AnalyseDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
